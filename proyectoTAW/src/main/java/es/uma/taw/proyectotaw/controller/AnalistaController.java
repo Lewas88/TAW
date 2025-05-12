3debb8ab6585da1d24664d2aa2cd17e2c471dcd6
Bfdiego <dayo.alba.p@gmail.com>
2025-05-12 16:56:37 +0200

    Pagina analista modificada

--- proyectoTAW/src/main/java/es/uma/taw/proyectotaw/controller/AnalistaController.java
+++ proyectoTAW/src/main/java/es/uma/taw/proyectotaw/controller/AnalistaController.java
@@ -1,7 +1,12 @@
 package es.uma.taw.proyectotaw.controller;
 
+import es.uma.taw.proyectotaw.dao.ActorRepository;
+import es.uma.taw.proyectotaw.dao.PeliculaRepository;
+import es.uma.taw.proyectotaw.dao.ReviewRepository;
+import es.uma.taw.proyectotaw.dao.UsuarioRepository;
 import es.uma.taw.proyectotaw.entity.UsuarioEntity;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,12 +16,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/analista")
 public class AnalistaController {
 
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+    @Autowired
+    private PeliculaRepository peliculaRepository;
+    @Autowired
+    private ReviewRepository reviewRepository;
+    @Autowired
+    private ActorRepository actorRepository;
+
     @GetMapping("/")
     public String doAnalista(Model model, HttpSession session) {
         UsuarioEntity usuario = (UsuarioEntity) session.getAttribute("user");
         if(usuario == null || usuario.getTipoUsuario().getId() != 3) {
             return "redirect:/login/";
         }
+        model.addAttribute("peliculas", peliculaRepository.findAll());
+        model.addAttribute("reviews", reviewRepository.findAll());
+        model.addAttribute("actores", actorRepository.findAll());
+        model.addAttribute("totalPeliculas", peliculaRepository.count());
+        model.addAttribute("totalReviews", reviewRepository.count());
+        model.addAttribute("totalActores", actorRepository.count());
+        model.addAttribute("totalUsuarios", usuarioRepository.count());
         model.addAttribute("usuario", usuario);
         return "analista";
     }
--- proyectoTAW/src/main/webapp/WEB-INF/view/analista.jsp
+++ proyectoTAW/src/main/webapp/WEB-INF/view/analista.jsp
@@ -12,5 +12,33 @@
 </head>
 <body>
 <jsp:include page="cabecera.jsp" />
+<%
+    Long totalPeliculas = (Long) request.getAttribute("totalPeliculas");
+    Long totalReviews = (Long) request.getAttribute("totalReviews");
+    Long totalActores = (Long) request.getAttribute("totalActores");
+    Long totalUsuarios = (Long) request.getAttribute("totalUsuarios");
+%>
+<table border="1">
+    <tr>
+        <th>Objeto</th>
+        <th>Cantidad</th>
+    </tr>
+    <tr>
+        <td>Peliculas</td>
+        <td><%= totalPeliculas %></td>
+    </tr>
+    <tr>
+        <td>Reviews</td>
+        <td><%= totalReviews %></td>
+    </tr>
+    <tr>
+        <td>Actores</td>
+        <td><%= totalActores %></td>
+    </tr>
+    <tr>
+        <td>Usuarios</td>
+        <td><%= totalUsuarios %></td>
+    </tr>
+</table>
 </body>
 </html>
